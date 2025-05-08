f75d967122a6528a5085db40e29eb20eae1562d9
Marcel <marcel.behar@gmail.com>
2025-05-07 22:28:52 -0400

    CORS modificacion

--- index.ts
+++ index.ts
@@ -2,8 +2,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import authRoutes from './auth';
+import cors from "cors";
+
+const corsOptions = {
+  origin: "https://subete-frontend.vercel.app", // ⚠️ ajusta según entorno
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
+};
 
 const app = express();
+app.use(cors(corsOptions));
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
