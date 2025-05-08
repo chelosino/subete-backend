f048f9ad653b2d3245b8e5bd5bf4131bbf1a34da
Marcel <marcel.behar@gmail.com>
2025-05-07 22:10:58 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import authRoutes from './auth';
+import { supabase } from "./supabase";
 import bodyParser from "body-parser";
 
 const app = express();
